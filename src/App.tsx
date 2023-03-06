37fbd6935db6d05e5b53decaad3d895bf38f6bb8
Katherina <fedoseeva2292@gmail.com>
2023-03-06 14:10:23 +0100

    Sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Calculator from './components/Calculator/Calculator';
 function App() {
   return (
     <Container maxW="2xl" centerContent>
-        <Grid
-          h="200px"
-          w='600px'
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(2, 1fr)"
-        >
-          <Calculator />
-        </Grid>
+      <Grid
+        h="200px"
+        w="600px"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 1fr)"
+      >
+        <Calculator />
+      </Grid>
     </Container>
   );
 }
--- src/components/LeftSide/SideBar/SideBar.tsx
+++ src/components/LeftSide/SideBar/SideBar.tsx
@@ -1,12 +1,27 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import React from 'react';
+import CalcButton from '../CalcButton/CalcButton';
+import Display from '../CalcDisplay/Display';
+import CalcNumbers from '../CalcNumbers/CalcNumbers';
+import CalcSigns from '../CalcSigns/CalcSigns';
 
 const SideBar = () => {
   return (
-    <Box bg='tomato' p={4} color='white'>
-      Sidebar
-    </Box>
-  )
-}
+    <Grid templateRows="repeat(4, auto)" gap={4}>
+      <GridItem w='100%'>
+        <Display />
+      </GridItem>
+      <GridItem w='100%'>
+        <CalcSigns />
+      </GridItem>
+      <GridItem w='100%'>
+        <CalcNumbers />
+      </GridItem>
+      <GridItem w='100%'>
+        <CalcButton />
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default SideBar
+export default SideBar;
