caf17521957316bf05d72a315b87562ec93b1ee9
Katherina <fedoseeva2292@gmail.com>
2023-03-06 16:28:21 +0100

    Added store and deps

--- src/components/LeftSide/CalcDisplay/Display.tsx
+++ src/components/LeftSide/CalcDisplay/Display.tsx
@@ -1,7 +1,11 @@
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
 
-const Display = () => {
+const Display: React.FC = () => {
+  const { value } = useSelector((state: RootState) => state.calc);
+
   return (
     <Box
       maxW="sm"
@@ -11,7 +15,7 @@ const Display = () => {
       textAlign={'right'}
     >
       <Text fontSize="2xl" p={1}>
-        Values
+        {value}
       </Text>
     </Box>
   );
